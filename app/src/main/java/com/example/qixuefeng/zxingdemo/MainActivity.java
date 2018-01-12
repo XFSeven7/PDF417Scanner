cf204912d16491597e5446a6c5810ecfd8718951
qixuefeng <xfseven7@foxmail.com>
2018-01-12 11:51:56 +0800

    special PDF417 scanner faster

--- app/src/main/java/com/example/qixuefeng/zxingdemo/MainActivity.java
+++ app/src/main/java/com/example/qixuefeng/zxingdemo/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.qixuefeng.zxingdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.zxing.client.android.CaptureActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void start(View view) {
+        Intent intent = new Intent(MainActivity.this, CaptureActivity.class);
+        startActivity(intent);
     }
 }
